73e325d6263b4d27907274c25facbd2c0d93cc79
Alejandrovalencia <avalenciaf@psl.com.co>
2020-07-02 14:12:43 -0500

    Page Object Model

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,11 +4,16 @@ export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
+  noGlobals: true,
   getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
     reporter();
     browser.ignoreSynchronization = true;
     browser.waitForAngularEnabled(false);
+    browser.manage().timeouts().implicitlyWait(5000);
   },
   capabilities: {
     browserName: 'chrome',
